2e41581e84691a4334c6a4acbdc377fc5346ccfd
ksfzhaohui <ksfzhaohui@126.com>
2016-04-05 20:02:48 +0800

    修改配置文件

--- excel2db-write/scripts/x-csharp-startup.bat
+++ excel2db-write/scripts/x-csharp-startup.bat
@@ -3,5 +3,7 @@ set language=csharp
 set beanRoot=D:\ndbtest\excelPath\bean
 set nameSpace=bean
 set excelPath=D:\ndbtest\excelPath
-java -jar excel2db-write-0.0.1-SNAPSHOT.jar %language% %beanRoot% %nameSpace% %excelPath%
+set ndbPath=D:\ndbtest\ndb
+
+java -jar excel2db-write-0.0.1-SNAPSHOT.jar %language% %beanRoot% %nameSpace% %excelPath% %ndbPath%
 pause
--- excel2db-write/scripts/x-java-startup.bat
+++ excel2db-write/scripts/x-java-startup.bat
@@ -3,5 +3,7 @@ set language=java
 set beanRoot=D:\ndbtest\excelPath\bean
 set packageRoot=test
 set excelPath=D:\ndbtest\excelPath
-java -jar excel2db-write-0.0.1-SNAPSHOT.jar %language% %beanRoot% %packageRoot% %excelPath%
+set ndbPath=D:\ndbtest\ndb
+
+java -jar excel2db-write-0.0.1-SNAPSHOT.jar %language% %beanRoot% %packageRoot% %excelPath% %ndbPath%
 pause
